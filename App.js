b2050cbea977807e1ddfe447bdd0323ac101cf13
AbhijeetRohilla <nishuinhurry@gmail.com>
2024-05-06 21:26:34 +0530

    completed 3rd  episode of Namaste javascript

--- App.js
+++ App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header/header';
 import RestCard from './components/body/restrauntCard';
+import './app.css'
 
 const App=()=>{
     
     return(
     <>
     <Header/>
+    <div className='restCard'>
     <RestCard/>
+    <RestCard/>
+    <RestCard/>
+    <RestCard/>
+    <RestCard/>
+    <RestCard/>
+    </div>
     </>
     ) 
 }
